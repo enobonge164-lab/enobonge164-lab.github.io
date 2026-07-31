1708056c772a14e84c389031f7583bb572502b8c
Sirwhite The Polymath <enobonge164@gmail.com>
2026-08-01 00:04:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -105,3 +105,25 @@ if (localStorage.getItem("theme") === "light") {
     document.body.classList.add("light-mode");
     themeToggle.textContent = "☀️";
 }
+// ================= BACK TO TOP =================
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
